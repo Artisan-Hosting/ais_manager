d349290308fec77cf670238f5927f471460bae8e
Darrion Whitfield <dwhitfield@ramfield.net>
2025-02-01 00:56:14 -0600

    overhaul

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,11 @@
 use applications::{
     child::{
         populate_initial_state_lock, spawn_client_applications, spawn_system_applications,
-        CLIENT_APPLICATION_HANDLER, SYSTEM_APPLICATION_HANDLER,
+        CLIENT_APPLICATION_ARRAY, CLIENT_APPLICATION_HANDLER, SYSTEM_APPLICATION_ARRAY,
+        SYSTEM_APPLICATION_HANDLER,
     },
-    monitor::{monitor_applications, update_application_usage},
-    resolve::{
-        resolve_client_applications, resolve_system_applications, Applications, ClientApplication,
-        SystemApplication,
-    },
+    monitor::{handle_dead_applications, handle_new_system_applications, monitor_application_resource_usage, track_application_uptime},
+    resolve::{resolve_client_applications, resolve_system_applications},
 };
 use artisan_middleware::dusa_collection_utils::log;
 use artisan_middleware::dusa_collection_utils::{
@@ -18,12 +16,12 @@ use artisan_middleware::{aggregator::AppStatus, config::AppConfig, state_persist
 use network::process_tcp;
 use std::{collections::HashMap, sync::Arc, time::Duration};
 use system::{
-    config::{generate_state, get_config}, control::{Controls, PortalState, PORTAL_CONTROLS}, portal::connect_with_portal, state::{get_state_path, save_state}
+    config::{generate_state, get_config},
+    control::{Controls, PortalState, PORTAL_CONTROLS},
+    portal::connect_with_portal,
+    state::{get_state_path, save_state},
 };
-use tokio::{
-    net::TcpListener,
-    time::sleep,
-};
+use tokio::{net::TcpListener, time::sleep};
 
 mod applications;
 mod network;
@@ -48,24 +46,31 @@ async fn main() -> Result<(), ErrorArrayItem> {
     application_controls.start_signal_monitors();
     application_controls.clone().start_contol_monitor();
 
+    // The functions ensure we only queue applications that we've verified we are supposed to run
+    if let Err(err) = resolve_system_applications().await {
+        log!(
+            LogLevel::Error,
+            "Failed to resolve system applications: {}",
+            err
+        );
+        application_controls.signal_shutdown();
+    };
 
-
-    // std::process::exit(0);
-
-    // Initiating array for applications.
-
-    // Initiating system applications.
-    // Initiating client appications.
-    let system_applications: Vec<SystemApplication> = resolve_system_applications().await;
-    let client_applications: Vec<ClientApplication> = resolve_client_applications(&config).await;
-    // print!("{:#?}", system_applications);
-    // print!("{:#?}", client_applications);
+    if let Err(err) = resolve_client_applications(&config).await {
+        log!(
+            LogLevel::Error,
+            "Failed to resolve client applications, running in safe mode: {}",
+            err
+        );
+        state.config.debug_mode = true;
+        state.config.environment = "systemonly".to_string();
+    }
 
     // Spawning applications
-    if config.environment != "systemonly" {
+    if state.config.environment != "systemonly" {
         spawn_client_applications(
             CLIENT_APPLICATION_HANDLER.clone(),
-            client_applications.clone(),
+            CLIENT_APPLICATION_ARRAY.clone(),
             &mut state,
             &state_path,
         )
@@ -74,49 +79,71 @@ async fn main() -> Result<(), ErrorArrayItem> {
 
     spawn_system_applications(
         SYSTEM_APPLICATION_HANDLER.clone(),
-        system_applications.clone(),
+        SYSTEM_APPLICATION_ARRAY.clone(),
         &mut state,
         &state_path,
     )
     .await?;
 
     // Adding applications to the status array
-    populate_initial_state_lock(Applications::System(system_applications.clone())).await?;
-
-    if config.environment != "systemonly" {
-        populate_initial_state_lock(Applications::Client(client_applications.clone())).await?;
-    }
+    populate_initial_state_lock(&mut state).await?;
 
     // Update metrics
+    let mut state_clone = state.clone();
+    let state_path_clone = state_path.clone();
     tokio::spawn(async move {
         loop {
-            if let Err(err) = monitor_applications(SYSTEM_APPLICATION_HANDLER.clone()).await {
+            if let Err(err) = handle_new_system_applications(&mut state_clone, &state_path_clone).await {
                 log!(LogLevel::Error, "{}", err);
             };
-            if let Err(err) = monitor_applications(CLIENT_APPLICATION_HANDLER.clone()).await {
+            if let Err(err) = monitor_application_resource_usage(SYSTEM_APPLICATION_HANDLER.clone()).await {
                 log!(LogLevel::Error, "{}", err);
             };
-            if let Err(err) = update_application_usage().await {
+            if let Err(err) = monitor_application_resource_usage(CLIENT_APPLICATION_HANDLER.clone()).await {
+                log!(LogLevel::Error, "{}", err);
+            };
+            if let Err(err) = track_application_uptime().await {
+                log!(LogLevel::Error, "{}", err);
+            }
+            if let Err(err) = handle_dead_applications().await {
                 log!(LogLevel::Error, "{}", err);
             }
             sleep(Duration::from_secs(2)).await;
         }
     });
 
-    // Regiser with portal 
+    // Regiser with portal
     let config_clone = config.clone();
     tokio::spawn(async move {
-        while !PortalState::is_portal_linked(PORTAL_CONTROLS.clone()).await.unwrap() {
-            log!(LogLevel::Debug, "Attempting to register with portals");
+        loop {
+            if !PortalState::is_portal_linked(PORTAL_CONTROLS.clone())
+                .await
+                .unwrap()
+            {
+                log!(LogLevel::Debug, "Attempting to register with portals");
 
-            if let Err(err) = connect_with_portal(&config_clone).await {
-                log!(LogLevel::Error, "{}", err)
+                if let Err(err) = connect_with_portal(&config_clone).await {
+                    log!(LogLevel::Error, "{}", err)
+                }
+
+                sleep(Duration::from_secs(30)).await;
             }
-
-            sleep(Duration::from_secs(30)).await;
-        }        
+        }
     });
 
+    // Updating the status from state files
+    // let config_clone = config.clone();
+    // let state_clone = state.clone();
+    // tokio::spawn(async move {
+        // loop {
+            // if let Err(err) = _update_application_state_from_disk(state_clone.clone(), &config_clone).await {
+                // log!(LogLevel::Error, "Error updating state from disks: {}", err);
+            // };
+// 
+            // sleep(Duration::from_secs(2)).await;
+        // }
+    // });
+// 
     // Initiating network stack
     let tcp_listener: TcpListener = TcpListener::bind(format!("0.0.0.0:9800"))
         .await
