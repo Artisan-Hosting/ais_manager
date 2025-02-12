bf70858ddaa1a8371404786f52bfd777ce1cce10
Darrion Whitfield <dwhitfield@ramfield.net>
2025-02-12 01:22:12 -0600

    _: time tuning and cargo

--- src/main.rs
+++ src/main.rs
@@ -39,27 +39,6 @@ async fn main() -> Result<(), ErrorArrayItem> {
         log!(LogLevel::Debug, "\n{}", state);
     }
 
-    // // loading previous app status array
-    // TODO Don't load application data from file, App failes to link, and update state data
-    // if let Ok(mut app_status_array_write_lock) = APP_STATUS_ARRAY.try_write().await {
-    //     match load_registered_apps().await {
-    //         Ok(arr) => {
-    //             for app in arr {
-    //                 app_status_array_write_lock.insert(app.clone().app_id, app);
-    //             }
-    //             drop(app_status_array_write_lock);
-    //         }
-    //         Err(err) => {
-    //             log!(LogLevel::Error, "{}", err);
-    //             log!(LogLevel::Info, "Creating new status tracking");
-    //             // Adding applications to the status array
-    //             drop(app_status_array_write_lock);
-    //             resolve_client_applications(&state.config).await?;
-    //             resolve_system_applications().await?;
-    //             populate_initial_state_lock(&mut state).await?;
-    //         }
-    //     }
-    // }
     {
         resolve_client_applications(&state.config).await?;
         resolve_system_applications().await?;
@@ -83,70 +62,41 @@ async fn main() -> Result<(), ErrorArrayItem> {
             if let Err(err) = handle_new_system_applications().await {
                 log!(LogLevel::Error, "{}", err);
             };
-            sleep(Duration::from_millis(287)).await;
+            sleep(Duration::from_millis(150)).await;
 
             if let Err(err) = handle_new_client_applications(&mut state_clone).await {
                 log!(LogLevel::Error, "{}", err);
             };
-            sleep(Duration::from_millis(287)).await;
+            sleep(Duration::from_millis(150)).await;
 
             if let Err(err) =
                 monitor_application_resource_usage(SYSTEM_APPLICATION_HANDLER.clone()).await
             {
                 log!(LogLevel::Error, "{}", err);
             };
-            sleep(Duration::from_millis(287)).await;
+            sleep(Duration::from_millis(150)).await;
 
             if let Err(err) =
                 monitor_application_resource_usage(CLIENT_APPLICATION_HANDLER.clone()).await
             {
                 log!(LogLevel::Error, "{}", err);
             };
-            sleep(Duration::from_millis(287)).await;
+            sleep(Duration::from_millis(150)).await;
 
             if let Err(err) = handle_dead_applications().await {
                 log!(LogLevel::Error, "{}", err);
             }
-            sleep(Duration::from_millis(287)).await;
+            sleep(Duration::from_millis(150)).await;
 
             if let Err(err) = update_client_state(&mut state_clone).await {
                 log!(LogLevel::Error, "{}", err);
             }
-            sleep(Duration::from_millis(287)).await;
+            sleep(Duration::from_millis(150)).await;
 
             if let Err(err) = update_system_state().await {
                 log!(LogLevel::Error, "{}", err);
             }
-            sleep(Duration::from_millis(287)).await;
-
-            // saving the application state to disk
-            // {
-            //     let app_status_array_read_lock = match APP_STATUS_ARRAY.try_read().await {
-            //         Ok(arr) => arr,
-            //         Err(err) => {
-            //             log!(LogLevel::Error, "{}", err);
-            //             continue;
-            //         }
-            //     };
-
-            //     let mut app_array: Vec<AppStatus> = Vec::new();
-
-            //     app_status_array_read_lock
-            //         .clone()
-            //         .into_iter()
-            //         .for_each(|app| {
-            //             app_array.push(app.1);
-            //         });
-
-            //     for app in app_array.clone() {
-            //         log!(LogLevel::Debug, "Status: {}", app);
-            //     }
-
-            //     if let Err(err) = save_registered_apps(&app_array).await {
-            //         log!(LogLevel::Error, "{}", err);
-            //     }
-            // }
-            // sleep(Duration::from_millis(287)).await;
+            sleep(Duration::from_millis(150)).await;
         }
     });
 
@@ -161,14 +111,6 @@ async fn main() -> Result<(), ErrorArrayItem> {
             sleep(Duration::from_secs(30)).await;
         }
     });
-
-    // tokio::spawn(async move {
-    //     sleep(Duration::from_secs(10)).await;
-    //     let read_app_status_lock = APP_STATUS_ARRAY.try_read().await.unwrap();
-    //     // clean_screen();
-    //     println!("App Status Array{:#?}", read_app_status_lock);
-    //     std::process::exit(0);
-    // });
 
     // Initiating network stack
     let tcp_listener: TcpListener = TcpListener::bind(format!("0.0.0.0:9800"))
