43931a90d52e02c124ec3547ae51eaa9aa18e2bb
Darrion Whitfield <dwhitfield@ramfield.net>
2025-04-11 23:58:27 +0000

    overhaul: NEW CONTROL SYSTEM

--- src/system/control.rs
+++ src/system/control.rs
@@ -1,139 +1,135 @@
-// Application control locks
-
+use std::collections::HashMap;
 use std::sync::RwLock;
+// Application control locks
 use std::{sync::Arc, time::Duration};
 
-use crate::applications::child::{
-    APP_STATUS_ARRAY, CLIENT_APPLICATION_HANDLER, SYSTEM_APPLICATION_HANDLER,
-};
-use crate::applications::resolve::{resolve_client_applications, resolve_system_applications};
-use artisan_middleware::aggregator::{save_registered_apps, AppStatus};
+use artisan_middleware::config::AppConfig;
 use artisan_middleware::dusa_collection_utils::errors::Errors;
 use artisan_middleware::dusa_collection_utils::logger::LogLevel;
+use artisan_middleware::dusa_collection_utils::types::pathtype::PathType;
 use artisan_middleware::dusa_collection_utils::types::rwarc::LockWithTimeout;
 use artisan_middleware::state_persistence::AppState;
 use artisan_middleware::{control::ToggleControl, dusa_collection_utils::errors::ErrorArrayItem};
 use artisan_middleware::{dusa_collection_utils::log, identity::Identifier};
-use once_cell::sync::Lazy;
 use tokio::sync::{Notify, OnceCell};
-use tokio::time::sleep;
 
+use super::config::{generate_state, get_config};
 use super::ebpf::BandwidthTracker;
 use super::portal::PortalAddr;
+use super::state::get_state_path;
 
-#[allow(dead_code)]
-pub static APPLICATION_CONTROLS: OnceCell<Arc<Controls>> = OnceCell::const_new();
-// pub static APPLICATION_CONTROLS: Lazy<Arc<Controls>> = Lazy::new(|| Arc::new(Controls::new()));
-pub static PORTAL_CONTROLS: Lazy<LockWithTimeout<PortalState>> =
-    Lazy::new(|| LockWithTimeout::new(PortalState::new()));
-// Diffrent locks for network communications and array locks
+pub static GLOBAL_STATE: OnceCell<Arc<GlobalState>> = OnceCell::const_new();
 
-/// Struct to manage control operations within the application
-pub struct Controls {
-    status_lock: Arc<ToggleControl>,
-    communication_lock: Arc<ToggleControl>,
-    reload_notify: Arc<Notify>,
-    shutdown_notify: Arc<Notify>,
-    kernel_app: BandwidthTracker,
+pub struct GlobalState {
+    pub signals: Arc<Signals>,
+    pub locks: Arc<Locks>,
+    pub portal_state: PortalState,
+    pub network_monitor: Arc<BandwidthTracker>,
+    pub app_state: Arc<RwLock<AppState>>,
+    pub app_state_path: PathType,
 }
 
-/// Struct to manage the portal's state and ensure proper linkage and timing
-#[derive(Clone, Debug)]
-pub struct PortalState {
-    portal_found: bool,
-    portal_addrs: Vec<PortalAddr>,
-    portal_identy: Option<Identifier>,
-    portal_linked: bool,
-    // portal_intime: bool,
-}
+impl GlobalState {
+    pub async fn initialize_global_state() -> Result<(), ErrorArrayItem> {
+        let signals: Arc<Signals> = Arc::new(Signals::new());
+        let locks: Arc<Locks> = Arc::new(Locks::new());
+        let portal_state: PortalState = PortalState::new()?;
+        let network_monitor: Arc<BandwidthTracker> = Arc::new(BandwidthTracker::new().await?);
 
-impl PortalState {
-    pub fn new() -> Self {
-        PortalState {
-            portal_found: false,
-            portal_addrs: vec![],
-            portal_identy: None,
-            portal_linked: false,
-            // portal_intime: false, // need a methode of tracking portal comms
+        let app_state_data: (Arc<RwLock<AppState>>, PathType) = {
+            let config: AppConfig = get_config();
+            let state: AppState = generate_state(&config).await;
+            let state_path: PathType = get_state_path(&config);
+
+            let wrapped_app_state = Arc::new(RwLock::new(state));
+            (wrapped_app_state, state_path)
+        };
+
+        let state: GlobalState = GlobalState {
+            portal_state,
+            network_monitor,
+            signals,
+            locks,
+            app_state: app_state_data.0,
+            app_state_path: app_state_data.1,
+        };
+
+        if let Err(err) = GLOBAL_STATE.set(Arc::new(state)) {
+            if err.is_already_init_err() {
+                log!(
+                    LogLevel::Warn,
+                    "Someone tried to re_initialize the global state"
+                );
+            }
+
+            if err.is_initializing_err() {
+                let err_item = ErrorArrayItem::new(
+                    Errors::AppState,
+                    format!("Error Initializing global state: {}", err.to_string()),
+                );
+                return Err(err_item);
+            }
+        }
+
+        Ok(())
+    }
+
+    pub async fn initialized(&self) -> bool {
+        if let None = GLOBAL_STATE.get() {
+            false
+        } else {
+            true
         }
     }
 
-    pub async fn set_identity(
-        id: Option<Identifier>,
-        portal_controls: LockWithTimeout<Self>,
-    ) -> Result<(), ErrorArrayItem> {
-        let mut portal_state_write_lock = portal_controls.try_write().await?;
-        portal_state_write_lock.portal_identy = id;
-        Ok(())
+    pub async fn get_identity(&self) -> Identifier {
+        self.portal_state.get_identity().await
     }
 
-    pub async fn _get_identity(
-        portal_controls: LockWithTimeout<Self>,
-    ) -> Result<Option<Identifier>, ErrorArrayItem> {
-        let portal_state_read_lock = portal_controls.try_read().await?;
-        Ok(portal_state_read_lock.portal_identy.clone())
-    }
-
-    pub async fn portal_addrs(
-        portal_controls: LockWithTimeout<Self>,
-    ) -> Result<Vec<PortalAddr>, ErrorArrayItem> {
-        let portal_state_read_lock = portal_controls.try_read().await?;
-        Ok(portal_state_read_lock.portal_addrs.clone())
-    }
-
-    pub async fn set_portal_addrs(
-        portal_controls: LockWithTimeout<Self>,
-        portals: Vec<PortalAddr>,
-    ) -> Result<(), ErrorArrayItem> {
-        let mut portal_state_write_lock = portal_controls.try_write().await?;
-        portal_state_write_lock.portal_addrs = portals;
-        portal_state_write_lock.portal_found = true;
-        drop(portal_state_write_lock);
-        Ok(())
-    }
-
-    pub async fn portal_linked(
-        portal_controls: LockWithTimeout<Self>,
-    ) -> Result<(), ErrorArrayItem> {
-        let mut portal_state_write_lock = portal_controls.try_write().await?;
-        portal_state_write_lock.portal_found = true;
-        Ok(())
-    }
-
-    pub async fn is_portal_linked(
-        portal_controls: LockWithTimeout<Self>,
-    ) -> Result<bool, ErrorArrayItem> {
-        let portal_state_read_lock = portal_controls.try_read().await?;
-        Ok(portal_state_read_lock.portal_linked.clone())
+    pub async fn get_state_clone(&self) -> Result<AppState, ErrorArrayItem> {
+        Ok(self.app_state
+        .try_read()
+        .map_err(|_| {
+            ErrorArrayItem::new(
+                Errors::AppState,
+                "Failed to get the app state from the global state",
+            )
+        })?
+        .clone())
     }
 }
 
-impl Controls {
-    async fn new() -> Result<Self, ErrorArrayItem> {
-        Ok(Self {
-            status_lock: Arc::new(ToggleControl::new()),
-            communication_lock: Arc::new(ToggleControl::new()),
+pub struct Signals {
+    pub reload_notify: Arc<Notify>,
+    pub shutdown_notify: Arc<Notify>,
+}
+
+impl Signals {
+    pub fn new() -> Self {
+        Self {
             reload_notify: Arc::new(Notify::new()),
             shutdown_notify: Arc::new(Notify::new()),
-            kernel_app: BandwidthTracker::new().await?,
-        })
+        }
     }
 
-    pub async fn initialize_controls() -> Result<(), ErrorArrayItem> {
-        let controls = Controls::new().await?;
-        APPLICATION_CONTROLS
-            .set(Arc::new(controls))
-            .map_err(|err| ErrorArrayItem::new(Errors::GeneralError, err.to_string()))?;
-        Ok(())
+    pub fn signal_shutdown(&self) {
+        self.shutdown_notify.notify_one();
     }
 
-    pub async fn get_controls() -> Result<Arc<Controls>, ErrorArrayItem> {
-        match APPLICATION_CONTROLS.get() {
-            Some(app) => Ok(app.clone()),
-            None => {
-                _ = Self::initialize_controls().await?;
-                Ok(APPLICATION_CONTROLS.get().unwrap().clone())
-            },
+    pub fn signal_reload(&self) {
+        self.reload_notify.notify_one();
+    }
+}
+
+pub struct Locks {
+    // status_lock: Arc<ToggleControl>, No need for it yet
+    communication_lock: Arc<ToggleControl>,
+}
+
+impl Locks {
+    pub fn new() -> Self {
+        Self {
+            communication_lock: Arc::new(ToggleControl::new()),
         }
     }
 
@@ -141,7 +137,7 @@ impl Controls {
     pub async fn wait_for_network_control(&self) {
         self.communication_lock.wait_if_paused().await;
     }
-    
+
     #[allow(dead_code)]
     pub async fn resume_network(&self) {
         self.communication_lock.resume();
@@ -158,169 +154,99 @@ impl Controls {
     ) -> Result<(), &str> {
         self.communication_lock.wait_with_timeout(timeout).await
     }
-
-    pub fn signal_reload(&self) {
-        self.reload_notify.notify_one();
-    }
-
-    /// Pauses all controls and ensures they are in a paused state.
-    pub async fn pause_all_controls(&self) -> bool {
-        self.communication_lock.wait_if_paused().await;
-        self.communication_lock.pause();
-        self.status_lock.wait_if_paused().await;
-        self.status_lock.pause();
-
-        self.communication_lock.is_paused().await && self.status_lock.is_paused().await
-    }
-
-    /// Pauses all controls and ensures they are in a paused state.
-    pub async fn resume_all_controls(&self) -> bool {
-        self.communication_lock.resume();
-        self.status_lock.resume();
-
-        !self.communication_lock.is_paused().await && !self.status_lock.is_paused().await
-    }
-
-    pub fn start_contol_monitor(self: Arc<Self>, state: AppState) {
-        tokio::spawn(async move {
-            loop {
-                tokio::select! {
-                    _ = self.reload_notify.notified() => {
-                        log!(LogLevel::Info, "Reloading");
-                        self.pause_all_controls().await;
-
-                        // Clearning the handlers
-                        let client_handler = &CLIENT_APPLICATION_HANDLER.clone();
-                        let system_handler = &SYSTEM_APPLICATION_HANDLER.clone();
-
-                        match client_handler.try_write().await {
-                            Ok(mut clients) => {
-                                clients.clear();
-                                clients.shrink_to_fit();
-                            },
-                            Err(err) => {
-                                log!(LogLevel::Error, "Failed to lock client handler, dumping: {}", err);
-                            },
-                        };
-
-                        match system_handler.try_write().await {
-                            Ok(mut systems) => {
-                                systems.clear();
-                                systems.shrink_to_fit();
-                            },
-                            Err(err) => {
-                                log!(LogLevel::Error, "Failed to lock system handler, dumping: {}", err);
-                            },
-                        };
-
-                        if let Err(err) = resolve_client_applications(&state.config).await {
-                            log!(LogLevel::Error, "{}", err);
-                        };
-
-                        if let Err(err) = resolve_system_applications().await {
-                            log!(LogLevel::Error, "{}", err);
-                        };
-
-                        log!(LogLevel::Info, "Reloaded !");
-                        self.resume_all_controls().await;
-                    }
-
-                    _ = self.shutdown_notify.notified() => {
-                        log!(LogLevel::Info, "Shutting down gracefully");
-                        sleep(Duration::from_millis(200)).await;
-
-                        self.pause_all_controls().await;
-
-                        // Clearning the handlers
-                        let client_handler = &CLIENT_APPLICATION_HANDLER.clone();
-                        let system_handler = &SYSTEM_APPLICATION_HANDLER.clone();
-
-                        match client_handler.try_write().await {
-                            Ok(mut clients) => {
-                                clients.clear();
-                                clients.shrink_to_fit();
-                            },
-                            Err(err) => {
-                                log!(LogLevel::Error, "Failed to lock client handler, dumping: {}", err);
-                            },
-                        };
-
-                        match system_handler.try_write().await {
-                            Ok(mut systems) => {
-                                systems.clear();
-                                systems.shrink_to_fit();
-                            },
-                            Err(err) => {
-                                log!(LogLevel::Error, "Failed to lock system handler, dumping: {}", err);
-                            },
-                        };
-
-                        // saving the system array to disk
-                        let app_status_array_read_lock = match APP_STATUS_ARRAY.try_read().await {
-                            Ok(arr) => arr,
-                            Err(err) => {
-                                log!(LogLevel::Error, "{}", err);
-                                continue;
-                            }
-                        };
-
-                        let mut app_array: Vec<AppStatus> = Vec::new();
-
-                        app_status_array_read_lock
-                            .clone()
-                            .into_iter()
-                            .for_each(|app| {
-                                app_array.push(app.1);
-                            });
-
-                        for app in app_array.clone() {
-                            log!(LogLevel::Debug, "Status: {}", app);
-                        }
-
-                        if let Err(err) = save_registered_apps(&app_array).await {
-                            log!(LogLevel::Error, "{}", err);
-                            std::process::exit(1)
-                        }
-
-                        log!(LogLevel::Info, "Bye~");
-                        std::process::exit(0)
-                    }
-
-                    _ = tokio::signal::ctrl_c() => {
-                        print!("\n"); // pretty in the terminal
-                        log!(LogLevel::Info, "CTRL + C recieved");
-                        self.shutdown_notify.notify_one();
-                    }
-                }
-            }
-        });
-    }
-
-    pub fn start_signal_monitors(&self) {
-        _signal_monitor(self.reload_notify.clone(), self.shutdown_notify.clone());
-    }
 }
 
-// Function to start signal monitors
-fn _signal_monitor(reload_notify: Arc<Notify>, shutdown_notify: Arc<Notify>) {
-    // Monitor SIGHUP for reload
-    tokio::spawn(async move {
-        let mut sighup = tokio::signal::unix::signal(tokio::signal::unix::SignalKind::hangup())
-            .expect("Failed to register SIGHUP");
-        while sighup.recv().await.is_some() {
-            log!(LogLevel::Info, "Received SIGHUP, signaling reload...");
-            reload_notify.notify_one();
-        }
-    });
+#[derive(Clone, Debug)]
+pub struct PortalIntance {
+    address: PortalAddr,
+    intime: bool, // we take note of the addr and time's it requests data
+}
 
-    // Monitor SIGUSR1 for shutdown
-    tokio::spawn(async move {
-        let mut sigusr1 =
-            tokio::signal::unix::signal(tokio::signal::unix::SignalKind::user_defined1())
-                .expect("Failed to register SIGUSR1");
-        while sigusr1.recv().await.is_some() {
-            log!(LogLevel::Info, "Received SIGUSR1, signaling shutdown...");
-            shutdown_notify.notify_one();
+impl PortalIntance {
+    pub fn new(address: PortalAddr) -> PortalIntance {
+        PortalIntance {
+            address,
+            intime: true,
         }
-    });
+    }
+
+    /// Sets a given instance as 'intime' meaning it's  requesting and getting data
+    pub fn in_time(&mut self) {
+        self.intime = true
+    }
+
+    /// Sets a given instance as 'outtime' meaning we've found it but it may be unhealthy
+    pub fn out_time(&mut self) {
+        self.intime = false
+    }
+
+    //  validates the identity of a given portal
+    // pub async fn validate(&self) -> bool {
+    //     self.identity.verify().await
+    // }
+}
+
+/// Struct to manage the portal's state and ensure proper linkage and timing
+#[derive(Clone, Debug)]
+pub struct PortalState {
+    identity: Identifier,
+    lock: LockWithTimeout<HashMap<PortalAddr, PortalIntance>>,
+}
+
+impl PortalState {
+    pub fn new() -> Result<Self, ErrorArrayItem> {
+        let array: HashMap<PortalAddr, PortalIntance> = HashMap::new();
+        let identity: Identifier = Identifier::load_from_file()?;
+        let lock: LockWithTimeout<HashMap<PortalAddr, PortalIntance>> = LockWithTimeout::new(array);
+        Ok(PortalState { identity, lock })
+    }
+
+    pub async fn insert(&self, address: PortalAddr) -> Result<(), ErrorArrayItem> {
+        let mut write_guard: tokio::sync::RwLockWriteGuard<'_, HashMap<PortalAddr, PortalIntance>> =
+            self.lock.try_write().await?;
+        let instance: PortalIntance = PortalIntance::new(address.clone());
+        write_guard.insert(address, instance);
+        Ok(())
+    }
+
+    pub async fn remove(
+        &self,
+        address: PortalAddr,
+    ) -> Result<Option<PortalIntance>, ErrorArrayItem> {
+        let mut write_guard: tokio::sync::RwLockWriteGuard<'_, HashMap<PortalAddr, PortalIntance>> =
+            self.lock.try_write().await?;
+        Ok(write_guard.remove(&address))
+    }
+
+    pub async fn intime(&self, address: PortalAddr, intime: bool) -> Result<(), ErrorArrayItem> {
+        let mut write_guard: tokio::sync::RwLockWriteGuard<'_, HashMap<PortalAddr, PortalIntance>> =
+            self.lock.try_write().await?;
+
+        match write_guard.get_mut(&address) {
+            Some(instance) => {
+                instance.intime = intime;
+                Ok(())
+            }
+            None => Err(ErrorArrayItem::new(
+                Errors::NotFound,
+                "refrenced portal instance not found ",
+            )),
+        }
+    }
+
+    pub async fn get_identity(&self) -> Identifier {
+        self.identity.clone()
+    }
+
+    pub async fn get_portals(&self) -> Result<Vec<PortalIntance>, ErrorArrayItem> {
+        let mut portal_array: Vec<PortalIntance> = Vec::new();
+        let read_guard: tokio::sync::RwLockReadGuard<'_, HashMap<PortalAddr, PortalIntance>> =
+            self.lock.try_read().await?;
+
+        read_guard.clone().into_values().for_each(|instance| {
+            portal_array.push(instance);
+        });
+
+        Ok(portal_array)
+    }
 }
