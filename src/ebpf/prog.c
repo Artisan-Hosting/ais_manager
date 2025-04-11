2095dad694735bb698cf47902b2d4a2737215f09
Darrion Whitfield <dwhitfield@ramfield.net>
2025-04-11 21:14:45 +0000

    clean: seperating macros to macros.h in the C code

--- src/ebpf/prog.c
+++ src/ebpf/prog.c
@@ -1,42 +1,7 @@
 #include "vmlinux.h"
 #include <bpf/bpf_helpers.h>
 #include <bpf/bpf_core_read.h>
-
-#ifndef PT_REGS_PARM3
-#if defined(__TARGET_ARCH_x86)
-  // For x86_64, the 3rd parameter is in rdx.
-  #define PT_REGS_PARM3(ctx) ((ctx)->dx)
-#elif defined(__TARGET_ARCH_arm64)
-  // For arm64, parameters are in the regs array; parameter 3 is at index 2.
-  #define PT_REGS_PARM3(ctx) ((ctx)->regs[2])
-#else
-  #error Unsupported target architecture!
-#endif
-#endif
-
-#ifndef PT_REGS_PARM4
-#if defined(__TARGET_ARCH_x86)
-  // For x86_64, the 4th parameter is in rdx.
-  #define PT_REGS_PARM4(ctx) ((ctx)->dx)
-#elif defined(__TARGET_ARCH_arm64)
-  // For arm64, parameters are in the regs array; parameter 3 is at index 2.
-  #define PT_REGS_PARM4(ctx) ((ctx)->regs[3])
-#else
-  #error Unsupported target architecture!
-#endif
-#endif
-
-#ifndef PT_REGS_PARM2
-#if defined(__TARGET_ARCH_x86)
-  // For x86_64, the 2nd parameter is in rdx.
-  #define PT_REGS_PARM2(ctx) ((ctx)->dx)
-#elif defined(__TARGET_ARCH_arm64)
-  // For arm64, parameters are in the regs array; parameter 3 is at index 2.
-  #define PT_REGS_PARM2(ctx) ((ctx)->regs[1])
-#else
-  #error Unsupported target architecture!
-#endif
-#endif
+#include "macros.h" 
 
 struct traffic_stats {
     __u64 rx_bytes;
