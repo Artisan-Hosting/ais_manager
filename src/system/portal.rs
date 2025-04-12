3d3ff0ddcade85e93e76afc4521ee671b674680e
Darrion Whitfield <dwhitfield@ramfield.net>
2025-04-12 08:23:58 +0000

    fix: revised portal fn's with global state

--- src/system/portal.rs
+++ src/system/portal.rs
@@ -13,22 +13,21 @@ use simple_comms::{
     network::send_receive::{send_empty_ok, send_message},
     protocol::{flags::Flags, proto::Proto},
 };
+use std::sync::Arc;
 use std::{
     fmt,
     net::{IpAddr, Ipv4Addr},
 };
 use tokio::net::TcpStream;
 
-use super::{
-    control::{PortalState, PORTAL_CONTROLS},
-    manager::get_manager_data,
-};
+use super::control::{GlobalState, GLOBAL_STATE};
+use super::manager::get_manager_data;
 
 #[allow(dead_code)]
-#[derive(Clone, Debug)]
+#[derive(Clone, Debug, Hash, PartialEq, Eq, PartialOrd, Ord)]
 pub struct PortalAddr {
-    addr: IpAddr,
-    port: u32,
+    pub addr: IpAddr,
+    pub port: u32,
 }
 
 impl fmt::Display for PortalAddr {
@@ -38,12 +37,6 @@ impl fmt::Display for PortalAddr {
 
         write!(f, "{}:{}", addr_colored, port_colored)
     }
-}
-
-async fn establish_portal_connection(address: &PortalAddr) -> Result<TcpStream, ErrorArrayItem> {
-    TcpStream::connect(format!("{}:{}", address.addr, address.port))
-        .await
-        .map_err(ErrorArrayItem::from)
 }
 
 async fn portal_discovery(stream: &mut TcpStream) -> Result<(), ErrorArrayItem> {
@@ -112,30 +105,19 @@ async fn handle_identity_response(
             if let Some(identifier) = identifier {
                 // * we are done communicating end the conn and finish up id work
                 let _ = send_empty_ok(stream, Proto::TCP).await;
-                if identifier.verify().await {
+                let a: Result<(), ErrorArrayItem> = if identifier.verify().await {
                     identifier.display_id();
                     identifier.save_to_file()?;
-                    PortalState::set_identity(Some(identifier), PORTAL_CONTROLS.clone()).await?;
                     Ok(())
                 } else {
                     Err(ErrorArrayItem::new(
                         Errors::AuthenticationError,
                         "Identifier verification failed".to_string(),
                     ))
-                }
+                };
+                a
             } else {
-                let identifier: Identifier = Identifier::load_from_file()?;
-                if identifier.verify().await {
-                    identifier.display_id();
-                    identifier.save_to_file()?;
-                    PortalState::set_identity(Some(identifier), PORTAL_CONTROLS.clone()).await?;
-                    Ok(())
-                } else {
-                    Err(ErrorArrayItem::new(
-                        Errors::AuthenticationError,
-                        "Identifier verification failed".to_string(),
-                    ))
-                }
+                Ok(())
             }
         }
         PortalMessage::Error(err) => Err(ErrorArrayItem::new(Errors::ConnectionError, err)),
@@ -156,7 +138,18 @@ pub async fn load_identifier() -> Option<Identifier> {
     }
 }
 
+/// Populates the global state with portal intances that we've found.
 async fn get_portal_addr(config: &AppConfig) -> Result<(), ErrorArrayItem> {
+    let global_state: &Arc<GlobalState> = match GLOBAL_STATE.get() {
+        Some(gs) => gs,
+        None => {
+            return Err(ErrorArrayItem::new(
+                Errors::AppState,
+                "Failed to get the app state from the global state",
+            ));
+        }
+    };
+
     let portal_port: u32 = 9801;
 
     let portal_addrs: Option<Vec<IpAddr>> = if config.environment == "development" {
@@ -167,22 +160,23 @@ async fn get_portal_addr(config: &AppConfig) -> Result<(), ErrorArrayItem> {
         .await
         .map_err(|err| ErrorArrayItem::new(Errors::Network, err.to_string()))?
     } else {
-        resolve_url("portal.arhst.net", Some(IpAddr::V4(Ipv4Addr::new(10, 1, 0, 1))))
-            .await
-            .map_err(|err| ErrorArrayItem::new(Errors::Network, err.to_string()))?
+        resolve_url(
+            "portal.arhst.net",
+            Some(IpAddr::V4(Ipv4Addr::new(10, 1, 0, 1))),
+        )
+        .await
+        .map_err(|err| ErrorArrayItem::new(Errors::Network, err.to_string()))?
     };
 
     if let Some(addrs) = portal_addrs {
-        let mut portals: Vec<PortalAddr> = Vec::new();
         for ip in addrs {
             let portal: PortalAddr = PortalAddr {
                 addr: ip,
                 port: portal_port,
             };
-            portals.push(portal);
+            global_state.portal_state.insert(portal).await?;
         }
 
-        PortalState::set_portal_addrs(PORTAL_CONTROLS.clone(), portals).await?;
         return Ok(());
     }
 
@@ -196,7 +190,7 @@ async fn portal_registration(
     mut stream: &mut TcpStream,
     data: ManagerData,
 ) -> Result<(), ErrorArrayItem> {
-    let message = PortalMessage::RegisterRequest(data);
+    let message: PortalMessage = PortalMessage::RegisterRequest(data);
 
     match send_message::<TcpStream, PortalMessage, PortalMessage>(
         &mut stream,
@@ -209,7 +203,8 @@ async fn portal_registration(
     {
         Ok(response) => match response.get_payload().await {
             PortalMessage::RegisterResponse(_res) => {
-                PortalState::portal_linked(PORTAL_CONTROLS.clone()).await?;
+                //PortalState::portal_linked(PORTAL_CONTROLS.clone()).await?;
+                // This should reset any out of time settings
                 return Ok(());
             }
             PortalMessage::Error(err) => {
@@ -236,35 +231,40 @@ async fn portal_registration(
 
 #[rustfmt::skip]
 pub async fn connect_with_portal(state:&mut AppState ) -> Result<(), ErrorArrayItem> {
-    if PortalState::is_portal_linked(PORTAL_CONTROLS.clone()).await? {
-        let addrs = PortalState::portal_addrs(PORTAL_CONTROLS.clone()).await?;
-        for addr in addrs {
-            log!(LogLevel::Debug, "Already linked to portal: {}", addr);
-        }        
-        return Ok(());
-    }
+    let global_state: &Arc<GlobalState> = match GLOBAL_STATE.get() {
+        Some(gs) => gs,
+        None => {
+            return Err(ErrorArrayItem::new(
+                Errors::AppState,
+                "Failed to get the app state from the global state",
+            ));
+        }
+    };
+
+
     
     get_portal_addr(&state.config).await?;
 
-    for portal_conn in PortalState::portal_addrs(PORTAL_CONTROLS.clone()).await? {
-        let mut stream: TcpStream = match establish_portal_connection(&portal_conn).await {
+    for portal in global_state.portal_state.get_portals().await? {
+        let mut stream: TcpStream = match portal.connect().await {
             Ok(s) => s,
             Err(err) => {
-                log!(LogLevel::Error, "Failed to connect to portal @ {} -> {}", portal_conn, err);
+                log!(LogLevel::Error, "Failed to connect to portal @ {} -> {}", portal.get_address(), err);
                 continue;
             }
         };
 
         if let Err(err) = portal_discovery(&mut stream).await {
-            log!(LogLevel::Error, "Failed to exchange identities with portal @ {} -> {}", portal_conn, err);
+            log!(LogLevel::Error, "Failed to exchange identities with portal @ {} -> {}", portal.get_address(), err);
         } else {
-            log!(LogLevel::Debug, "Discovered @ {} !", portal_conn);
+            log!(LogLevel::Debug, "Discovered @ {} !", portal.get_address());
         }
 
-        let mut stream: TcpStream = match establish_portal_connection(&portal_conn).await {
+        // * Due to a poor protocol implemtation we need to re initialize the connection here
+        let mut stream: TcpStream = match portal.connect().await {
             Ok(s) => s,
             Err(err) => {
-                log!(LogLevel::Error, "Failed to connect to portal @ {} -> {}", portal_conn, err);
+                log!(LogLevel::Error, "Failed to connect to portal @ {} -> {}", portal.get_address(), err);
                 continue;
             }
         };
@@ -272,10 +272,10 @@ pub async fn connect_with_portal(state:&mut AppState ) -> Result<(), ErrorArrayI
         match get_manager_data(state).await {
             Ok(data) => {
             if let Err(err) = portal_registration(&mut stream, data).await {
-                log!(LogLevel::Error, "Failed to register with portal @ {} -> {}", portal_conn, err);
+                log!(LogLevel::Error, "Failed to register with portal @ {} -> {}", portal.get_address(), err);
             } else {
-                log!(LogLevel::Debug, "Registered with portal @ {} !", portal_conn);
-                PortalState::portal_linked(PORTAL_CONTROLS.clone()).await?;
+                log!(LogLevel::Debug, "Registered with portal @ {} !", portal.get_address());
+                global_state.portal_state.set_time(portal.get_address(), true).await?;
             }
             },
             Err(err) => {
