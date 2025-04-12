99b4682ccb26f3bd57531b0304179f12a155b6f1
Darrion Whitfield <dwhitfield@ramfield.net>
2025-04-12 10:48:14 +0000

    chore: re_organizing macros

--- src/ebpf/macros.h
+++ src/ebpf/macros.h
@@ -1,15 +1,3 @@
-#ifndef PT_REGS_PARM3
-#if defined(__TARGET_ARCH_x86)
-  // For x86_64, the 3rd parameter is in rdx.
-  #define PT_REGS_PARM3(ctx) ((ctx)->dx)
-#elif defined(__TARGET_ARCH_arm64)
-  // For arm64, parameters are in the regs array; parameter 3 is at index 2.
-  #define PT_REGS_PARM3(ctx) ((ctx)->regs[2])
-#else
-  #error Unsupported target architecture!
-#endif
-#endif
-
 #ifndef PT_REGS_PARM4
 #if defined(__TARGET_ARCH_x86)
   // For x86_64, the 4th parameter is in rdx.
@@ -17,6 +5,18 @@
 #elif defined(__TARGET_ARCH_arm64)
   // For arm64, parameters are in the regs array; parameter 3 is at index 2.
   #define PT_REGS_PARM4(ctx) ((ctx)->regs[3])
+#else
+  #error Unsupported target architecture!
+#endif
+#endif
+
+#ifndef PT_REGS_PARM3
+#if defined(__TARGET_ARCH_x86)
+  // For x86_64, the 3rd parameter is in rdx.
+  #define PT_REGS_PARM3(ctx) ((ctx)->dx)
+#elif defined(__TARGET_ARCH_arm64)
+  // For arm64, parameters are in the regs array; parameter 3 is at index 2.
+  #define PT_REGS_PARM3(ctx) ((ctx)->regs[2])
 #else
   #error Unsupported target architecture!
 #endif
